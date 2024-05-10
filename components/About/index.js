4f1bd5f43db9590da849df70a6f0c9eb54fe7bb7
Ziyuan Lin <zl4140@nyu.edu>
2024-05-10 19:29:39 -0400

    change style to tailwind

--- components/About/index.js
+++ components/About/index.js
@@ -1,21 +1,14 @@
 export default function About() {
   return (
     <div id="about" className="p-14 pt-48 min-h-screen sm:p-24 sm:pt-48">
-      <h1 className="text-center font-bold text-4xl pb-12 sm:text-left">
-        About
-      </h1>
+      <h1 className="text-center font-bold text-4xl pb-12 sm:text-left">About</h1>
       <p className="text-center pb-12 sm:text-left">
-        We are training an ethical, open-source Large Language Model (LLM) using
-        20+ years of student work from the International Telecommunications
-        Program (ITP) at NYU Tisch. Our goal is to reflect the creative legacy
-        of the ITP community with a model that can reflect on the history of
-        student work, participate in classes, and generate new work based on the
-        creative consciousness of ITP.
+        We are training an ethical, open-source Large Language Model (LLM) using 20+ years of
+        student work from the International Telecommunications Program (ITP) at NYU Tisch. Our goal
+        is to reflect the creative legacy of the ITP community with a model that can reflect on the
+        history of student work, participate in classes, and generate new work based on the creative
+        consciousness of ITP.
       </p>
-      {/* <p>Follow us on social Media!</p> */}
-      <button className="btn-pos border border-white text-white font-bold p-4 w-48  hover:bg-white hover:text-black">
-        Database
-      </button>
     </div>
   );
 }
--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,38 +3,24 @@ export default function Footer() {
   return (
     <div
       id="footer"
-      className="left-0 top-0 flex w-full items-center justify-between p-4 footer-background"
+      className="left-0 top-0 flex w-full items-center justify-between p-4 bg-gray-800"
     >
-      <div id="socio-container">
+      <div className="relative m-auto">
         <ul className="flex gap-14">
-        <a href="chat.itp@itp.nyu.edu" target="_blank" rel="noopener noreferrer">
-          <Image
-                src="/envelope.png"
-                alt="Envelope icon"
-                width={25}
-                height={25}
-            />
-        </a>
-            <a href="https://www.linkedin.com/company/chat-itp/" target="_blank" rel="noopener noreferrer">
-                <Image
-                src="/linkedin.png"
-                alt="Description of the image"
-                width={25}
-                height={25}
-                />
-            </a>
-            <a href="https://www.instagram.com/chat.itp/" target="_blank" rel="noopener noreferrer">
-                <Image
-                src="/instagram.png"
-                alt="Description of the image"
-                width={25}
-                height={25}
-                />
-            </a>
-            
-            
+          <a href="mailto: chat.itp@itp.nyu.edu" target="_blank" rel="noopener noreferrer">
+            <Image src="/envelope.png" alt="Envelope icon" width={25} height={25} />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/chat-itp/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image src="/linkedin.png" alt="Description of the image" width={25} height={25} />
+          </a>
+          <a href="https://www.instagram.com/chat.itp/" target="_blank" rel="noopener noreferrer">
+            <Image src="/instagram.png" alt="Description of the image" width={25} height={25} />
+          </a>
         </ul>
-            
       </div>
     </div>
   );
