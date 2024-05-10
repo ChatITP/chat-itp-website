eaf9fdd681e0d4572c15fc11d0ff81d9f085905f
Ziyuan Lin <zl4140@nyu.edu>
2024-05-10 19:32:54 -0400

    move footer to layout

--- app/page.js
+++ app/page.js
@@ -1,13 +1,11 @@
 import Home from "../components/Home";
 import About from "../components/About";
-import Footer from "../components/Footer"
 
 export default function Page() {
   return (
     <main>
       <Home />
       <About />
-      <Footer />
     </main>
   );
 }
