6f26bcf9bfd7757b1a87086c9d9974fc254cfa2b
nina-jsl <145054713+nina-jsl@users.noreply.github.com>
2024-05-11 18:49:47 -0400

    submit-form

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Home from "../components/Home";
 import About from "../components/About";
+import Form from "../pages";
+
 
 export default function Page() {
   return (
     <main>
       <Home />
       <About />
+      <Form />
     </main>
   );
 }
