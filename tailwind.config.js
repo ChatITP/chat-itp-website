8c725d6d989f26024cae8e6ad50bfc40040de4b5
Ziyuan Lin <zl4140@nyu.edu>
2024-05-10 23:46:43 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,15 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      white: "#ffffff",
+      black: "#000000",
+      purple: "#b8b8d1",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
