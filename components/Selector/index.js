4ed0773c83581295af1e8d77e717c836a61f91e4
nina-jsl <145054713+nina-jsl@users.noreply.github.com>
2024-06-24 13:31:01 +0800

    created conversation page

--- components/Selector/index.js
+++ components/Selector/index.js
@@ -37,27 +37,29 @@ const Selector = () => {
               </div>
             </li>
           </Link>
-
-          <li
-            onClick={() => handleSectionClick("Home")}
-            className="cursor-pointer"
-          >
-            <div
-              className={`flex items-center justify-center p-2 w-[60px] h-[40px] rounded-full hover:bg-gradient-to-r from-main/70 to-purple/5 hover:border-[1.5px] hover:border-gray-100 ${
-                activeSection === "Convo"
-                  ? "bg-gradient-to-r from-main/70 to-purple/5 border-[1.5px] border-gray-100"
-                  : ""
-              }`}
+          <Link href="/Conversation">
+            <li
+              onClick={() => handleSectionClick("Home")}
+              className="cursor-pointer"
             >
-              <Image
-                src="/convo.svg"
-                alt="Convo Icon"
-                className="block object-cover w-[15px] lg:w-[20px]"
-                width={20}
-                height={20}
-              />
-            </div>
-          </li>
+              <div
+                className={`flex items-center justify-center p-2 w-[60px] h-[40px] rounded-full hover:bg-gradient-to-r from-main/70 to-purple/5 hover:border-[1.5px] hover:border-gray-100 ${
+                  activeSection === "Convo"
+                    ? "bg-gradient-to-r from-main/70 to-purple/5 border-[1.5px] border-gray-100"
+                    : ""
+                }`}
+              >
+                <Image
+                  src="/convo.svg"
+                  alt="Convo Icon"
+                  className="block object-cover w-[15px] lg:w-[20px]"
+                  width={20}
+                  height={20}
+                />
+              </div>
+            </li>
+          </Link>
+
           <li
             onClick={() => handleSectionClick("Question")}
             className="cursor-pointer"
