ce5138f673452195ff55b0f618a083fed577de4a
nina-jsl <145054713+nina-jsl@users.noreply.github.com>
2024-06-24 12:14:00 +0800

    included logo

--- components/Nav/index.js
+++ components/Nav/index.js
@@ -1,17 +1,33 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Nav() {
   return (
-    <nav className="z-50 fixed left-0 top-0 flex w-full items-center justify-between p-4 bg-white/10 backdrop-blur-md">
-      <h1 className="text-lg font-bold">Chat ITP</h1>
-      <ul className="flex gap-4">
+    <nav className="z-50 fixed left-0 top-0 flex flex-col-2 w-full">
+      <ul>
         <li>
-          <Link href="#home">Home</Link>
+          <Image
+            src="/logo.png"
+            alt="ChatITP LOGO"
+            className="block object-cover w-full h-auto left-[36px]"
+            width={70}
+            height={70}
+          />
         </li>
-        <li>
-          <Link href="#about">About</Link>
-        </li>
+        <li></li>
       </ul>
     </nav>
+
+    // <nav className="z-50 fixed left-0 top-0 flex w-full items-center justify-between p-4 bg-white/10 backdrop-blur-md">
+    //   <h1 className="text-lg font-bold">Chat ITP</h1>
+    //   <ul className="flex gap-4">
+    //     <li>
+    //       <Link href="#home">Home</Link>
+    //     </li>
+    //     <li>
+    //       <Link href="#about">About</Link>
+    //     </li>
+    //   </ul>
+    // </nav>
   );
 }
