5e2b30252ecf344d94475d6c671a4e3ce528e97a
nina-jsl <145054713+nina-jsl@users.noreply.github.com>
2024-07-16 20:39:02 +0800

    added about page and changed landing page to convo

---
+++ app/about/page.js
@@ -0,0 +1,13 @@
+import Home from "@/components/Home";
+import About from "@/components/About";
+import Post from "@/components/Post"
+
+export default function Page() {
+  return (
+    <main>
+      <Home />
+      <About />
+      <Post />
+    </main>
+  );
+}
