5589c5903343402aad61e79033a2cabbf3c57f32
nina-jsl <145054713+nina-jsl@users.noreply.github.com>
2024-07-19 16:45:14 +0800

    updated draggable chatinterface

--- components/Convo/DraggableChatInterface.js
+++ components/Convo/DraggableChatInterface.js
@@ -1,18 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ChatInterface from './ChatInterface'; // Adjust the path if necessary
 
-const DraggableChatInterface = ({
-  initialX,
-  initialY,
-  isSelected,
-  onClick,
-}) => {
+const DraggableChatInterface = ({ id, initialX, initialY, isSelected, onClick }) => {
+  const [position, setPosition] = useState({ x: initialX, y: initialY });
+  const [isDragging, setIsDragging] = useState(false);
+  const positionRef = useRef(position);
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      if (isDragging) {
+        const newX = positionRef.current.x + event.movementX;
+        const newY = positionRef.current.y + event.movementY;
+        setPosition({ x: newX, y: newY });
+      }
+    };
+
+    const handleMouseUp = () => {
+      setIsDragging(false);
+    };
+
+    if (isDragging) {
+      window.addEventListener('mousemove', handleMouseMove);
+      window.addEventListener('mouseup', handleMouseUp);
+    } else {
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('mouseup', handleMouseUp);
+    }
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, [isDragging]);
+
+  useEffect(() => {
+    positionRef.current = position;
+  }, [position]);
+
+  const handleMouseDown = (event) => {
+    event.stopPropagation();
+    setIsDragging(true);
+  };
+
   return (
     <div
       style={{
         position: 'absolute',
-        left: initialX,
-        top: initialY,
+        left: position.x,
+        top: position.y,
         cursor: 'pointer',
         zIndex: 1,
         border: isSelected ? '2px solid blue' : 'none',
@@ -20,6 +55,7 @@ const DraggableChatInterface = ({
         overflow: 'hidden',
       }}
       className={`${isSelected ? 'shadow-lg' : ''}`}
+      onMouseDown={handleMouseDown}
       onClick={(e) => {
         e.stopPropagation();
         onClick();
@@ -31,9 +67,4 @@ const DraggableChatInterface = ({
 };
 
 export default DraggableChatInterface;
-
-
-
-
-
 
