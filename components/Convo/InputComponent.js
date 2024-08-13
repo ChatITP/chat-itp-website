e7cc143c1fa6dce55ae970fc3e3271f1ab3141a6
nina-jsl <145054713+nina-jsl@users.noreply.github.com>
2024-08-13 11:42:43 +0800

    editable tag

--- components/Convo/InputComponent.js
+++ components/Convo/InputComponent.js
@@ -5,7 +5,6 @@ import DropZone from "./DropZone";
 import Image from "next/image";
 import Link from "next/link";
 import Background from "./Background";
-import Switch from "./Switch";
 import Phrase from "./Phrase";
 
 const ItemType = {
@@ -29,7 +28,37 @@ const tagStyles = {
   fontFamily: "'Open Sans', sans-serif",
 };
 
-const InputComponent = ({ initialTags = [], phrases = [] }) => {
+const escapeRegExp = (string) => {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+};
+
+const countHighlights = (text, highlights) => {
+  if (!highlights.length) return 0;
+  const escapedHighlights = highlights.map(escapeRegExp);
+  const regex = new RegExp(`(${escapedHighlights.join("|")})`, "gi");
+  const matches = text.match(regex);
+  return matches ? matches.length : 0;
+};
+
+const highlightText = (text, highlights) => {
+  if (!highlights.length) return text;
+  const escapedHighlights = highlights.map(escapeRegExp);
+  const regex = new RegExp(`(${escapedHighlights.join("|")})`, "gi");
+  const parts = text.split(regex);
+  return parts.map((part, index) =>
+    escapedHighlights.some(
+      (highlight) => part.toLowerCase() === highlight.toLowerCase()
+    ) ? (
+      <span key={index} className="bg-blue text-white rounded-lg px-1">
+        {part}
+      </span>
+    ) : (
+      part
+    )
+  );
+};
+
+const InputComponent = ({ phrases = [] }) => {
   const [searchKey, setSearchKey] = useState("");
   const [items, setItems] = useState([]);
   const [clickedPosition, setClickedPosition] = useState({ x: 0, y: 0 });
@@ -60,7 +89,6 @@ const InputComponent = ({ initialTags = [], phrases = [] }) => {
   ];
 
   const [defaultTags, setDefaultTags] = useState([]);
-  const [personalizedTags, setPersonalizedTags] = useState([...initialTags]);
   const [selectedTags, setSelectedTags] = useState([]);
 
   useEffect(() => {
@@ -75,73 +103,15 @@ const InputComponent = ({ initialTags = [], phrases = [] }) => {
   };
 
   const generateRandomDefaultTags = () => {
-    const shuffledTags = [...defaultTagsPool].sort(() => 0.5 - Math.random());
-    setDefaultTags(shuffledTags.slice(0, 6));
-    setSelectedTags([]);
-  };
-
-  const handleKeyDown = (e) => {
-    if (e.key === "Enter" && searchKey.trim() !== "") {
-      const newTag = searchKey.trim();
-      const newPersonalizedTags = [...personalizedTags, newTag];
-      const updatedSelectedTags = [...selectedTags, newTag];
-
-      setPersonalizedTags(newPersonalizedTags);
-      setSelectedTags(updatedSelectedTags);
-      setSearchKey("");
-      setShowInput(false);
-      setTimeout(() => {
-        if (tagsContainerRef.current) {
-          tagsContainerRef.current.scrollLeft =
-            tagsContainerRef.current.scrollWidth;
-        }
-      }, 100);
-    }
-  };
-
-  const handleChange = (e) => {
-    setSearchKey(e.target.value);
-  };
-
-  const removeTag = (tagToRemove, isPersonalized) => {
-    if (isPersonalized) {
-      setPersonalizedTags(
-        personalizedTags.filter((tag) => tag !== tagToRemove)
-      );
-    } else {
-      setDefaultTags(defaultTags.filter((tag) => tag !== tagToRemove));
-    }
-    setSelectedTags(selectedTags.filter((tag) => tag !== tagToRemove));
-  };
-
-  const escapeRegExp = (string) => {
-    return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-  };
-
-  const highlightText = (text, highlights) => {
-    if (!highlights.length) return text;
-    const escapedHighlights = highlights.map(escapeRegExp);
-    const regex = new RegExp(`(${escapedHighlights.join("|")})`, "gi");
-    const parts = text.split(regex);
-    return parts.map((part, index) =>
-      escapedHighlights.some(
-        (highlight) => part.toLowerCase() === highlight.toLowerCase()
-      ) ? (
-        <span key={index} className="bg-blue text-white rounded-lg px-1">
-          {part}
-        </span>
-      ) : (
-        part
-      )
+    const unselectedTags = defaultTagsPool.filter(
+      (tag) => !selectedTags.includes(tag)
     );
-  };
-
-  const countHighlights = (text, highlights) => {
-    if (!highlights.length) return 0;
-    const escapedHighlights = highlights.map(escapeRegExp);
-    const regex = new RegExp(`(${escapedHighlights.join("|")})`, "gi");
-    const matches = text.match(regex);
-    return matches ? matches.length : 0;
+    const shuffledTags = [...unselectedTags].sort(() => 0.5 - Math.random());
+    setDefaultTags([
+      ...selectedTags,
+      ...shuffledTags.slice(0, 5 - selectedTags.length),
+      "Add Custom Tag",
+    ]);
   };
 
   const handleTagClick = (tag) => {
@@ -150,15 +120,6 @@ const InputComponent = ({ initialTags = [], phrases = [] }) => {
         ? prevSelectedTags.filter((t) => t !== tag)
         : [...prevSelectedTags, tag]
     );
-  };
-
-  const handleClick = (item) => {
-    clickedItemRef.current = item;
-  };
-
-  const clearTags = () => {
-    setPersonalizedTags([]);
-    setSelectedTags([]);
   };
 
   const handleTagDoubleClick = (index, currentText) => {
@@ -170,32 +131,29 @@ const InputComponent = ({ initialTags = [], phrases = [] }) => {
     setEditingText(e.target.value);
   };
 
-  const handleEditKeyDown = (e, index, isPersonalized) => {
+  const handleEditKeyDown = (e, index) => {
     if (e.key === "Enter") {
-      saveEditedTag(index, isPersonalized);
+      saveEditedTag(index);
     }
   };
 
-  const saveEditedTag = (index, isPersonalized) => {
-    if (isPersonalized) {
-      const newPersonalizedTags = [...personalizedTags];
-      newPersonalizedTags[index] = editingText;
-      setPersonalizedTags(newPersonalizedTags);
-      setModifiedTags((prev) => new Set(prev).add(newPersonalizedTags[index]));
-    } else {
-      const newDefaultTags = [...defaultTags];
-      newDefaultTags[index] = editingText;
-      setDefaultTags(newDefaultTags);
-      setModifiedTags((prev) => new Set(prev).add(newDefaultTags[index]));
-    }
+  const saveEditedTag = (index) => {
+    const newDefaultTags = [...defaultTags];
+    newDefaultTags[index] = editingText;
+    setDefaultTags(newDefaultTags);
+    setModifiedTags((prev) => new Set(prev).add(newDefaultTags[index]));
     setEditingTagIndex(null);
+  };
+
+  const handleClick = (item) => {
+    clickedItemRef.current = item;
   };
 
   const [, drop] = useDrop({
     accept: ItemType.PHRASE,
     drop: (item, monitor) => {
       const delta = monitor.getClientOffset();
-      clickedItemRef.current = item.phrase; // Update ref instead of state
+      clickedItemRef.current = item.phrase;
       setClickedPosition({
         x: delta.x - CHAT_WINDOW_DIMENSIONS.width,
         y: delta.y - CHAT_WINDOW_DIMENSIONS.height,
@@ -242,10 +200,15 @@ const InputComponent = ({ initialTags = [], phrases = [] }) => {
                       key={index}
                       onClick={() => handleTagClick(tag)}
                       onDoubleClick={() => handleTagDoubleClick(index, tag)}
-                      style={tagStyles}
+                      style={{
+                        ...tagStyles,
+                        width: tag === "Add Custom Tag" ? "120px" : tagStyles.width, 
+                      }}
                       className={`cursor-pointer text-xs ${
                         selectedTags.includes(tag)
                           ? "bg-lightBlue text-blue"
+                          : tag === "Add Custom Tag"
+                          ? "bg-white/20 border underline border-white/20 text-white/50 w-32"
                           : "bg-none border border-white text-white"
                       } font-semibold rounded-lg px-3 py-1 whitespace-nowrap`}
                     >
@@ -254,86 +217,17 @@ const InputComponent = ({ initialTags = [], phrases = [] }) => {
                           type="text"
                           value={editingText}
                           onChange={handleEditChange}
-                          onKeyDown={(e) => handleEditKeyDown(e, index, false)}
-                          onBlur={() => saveEditedTag(index, false)}
-                          className="w-full h-full text-center bg-none border-none outline-none"
+                          onKeyDown={(e) => handleEditKeyDown(e, index)}
+                          onBlur={() => saveEditedTag(index)}
+                          className="w-full h-full text-center bg-yellow/80 border-none outline-none"
                         />
                       ) : (
                         <span>{tag}</span>
                       )}
-                      {/* {selectedTags.includes(tag) && (
-                        <button
-                          type="button"
-                          className="ml-2 text-lightBlue bg-blue w-4 h-4 flex items-center justify-center rounded-full"
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            removeTag(tag, false);
-                          }}
-                        >
-                          &times;
-                        </button>
-                      )} */}
                     </div>
                   ))}
-                {personalizedTags.length > 0 &&
-                  personalizedTags.map((tag, index) => (
-                    <div
-                      key={index}
-                      onClick={() => handleTagClick(tag)}
-                      onDoubleClick={() => handleTagDoubleClick(index, tag)}
-                      style={tagStyles}
-                      className={`cursor-pointer text-xs ${
-                        selectedTags.includes(tag)
-                          ? "bg-lightBlue text-blue"
-                          : "bg-none border border-white text-white"
-                      } font-semibold rounded-lg px-3 py-1 whitespace-nowrap`}
-                    >
-                      {editingTagIndex === index ? (
-                        <input
-                          type="text"
-                          value={editingText}
-                          onChange={handleEditChange}
-                          onKeyDown={(e) => handleEditKeyDown(e, index, true)}
-                          onBlur={() => saveEditedTag(index, true)}
-                          className="w-full h-full text-center bg-none border-none outline-none"
-                        />
-                      ) : (
-                        <span>{tag}</span>
-                      )}
-                      {/* {selectedTags.includes(tag) && (
-                        <button
-                          type="button"
-                          className="ml-2 text-lightBlue bg-blue w-4 h-4 flex items-center justify-center rounded-full"
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            removeTag(tag, true);
-                          }}
-                        >
-                          &times;
-                        </button>
-                      )} */}
-                    </div>
-                  ))}
-                {showInput && (
-                  <input
-                    className="bg-gray outline-none text-white pl-2 text-xs"
-                    value={searchKey}
-                    onChange={handleChange}
-                    onKeyDown={handleKeyDown}
-                    placeholder="Add Custom Keyword"
-                    onBlur={() => setShowInput(false)}
-                  />
-                )}
               </div>
               <div className="flex items-center ml-2 space-x-2">
-                {!showInput && (
-                  <button
-                    className="bg-none border border-white text-white/50 font-semibold py-1 px-3 rounded-md text-xs underline decoration-solid"
-                    onClick={() => setShowInput(true)}
-                  >
-                    Add Custom Keyterm
-                  </button>
-                )}
                 <Image
                   src="/switch.svg"
                   alt="switch icon"
