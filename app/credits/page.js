c9e979fa993446aa95fc8a9836db4d05decbfe64
nina-jsl <145054713+nina-jsl@users.noreply.github.com>
2024-08-19 13:07:10 +0800

    style change

--- app/credits/page.js
+++ app/credits/page.js
@@ -5,7 +5,6 @@ import Link from "next/link";
 const CreditPage = () => {
   return (
     <>
-    
       <div className="bg-gradient-to-b from-[#1b0e25] via-[#1b0e25] to-[#3a2238]">
         <nav className="px-2 pt-4 h-[84px] z-10">
           <Link href="/">
@@ -21,7 +20,14 @@ const CreditPage = () => {
 
         <div id="acknowledge" className="pt-[84px] h-screen mx-[130px] z-10">
           <div id="title">
-            <h1 className="text-center text-4xl font-sans font-bold mb-7 bg-gradient-to-r from-[#FFC75F] via-[#A8C4FF] to-[#FF6A88] bg-clip-text text-transparent">
+            <h1
+              className="text-center text-4xl font-sans font-bold mb-7 text-transparent bg-clip-text"
+              style={{
+                backgroundImage: "url('/radialBg.jpg')",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            >
               Credits and Acknowledgments
             </h1>
           </div>
@@ -32,7 +38,15 @@ const CreditPage = () => {
             </p>
           </div>
           <div className="flex flex-row gap-6">
-            <div className="w-[750px] h-[402px] bg-custom-radial rounded-3xl">
+            <div
+              className="w-[750px] h-[402px] rounded-3xl"
+              style={{
+                backgroundImage: "url('/radialBg.jpg')",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                opacity: 0.8,
+              }}
+            >
               <div className="w-[738px] h-[390px] bg-gradient-to-b from-[#1b0e25] via-[#1b0e25] to-[#3a2238] rounded-3xl m-auto mt-[6px]">
                 <p className="text-xl font-sans font-bold text-center pt-8 mb-6">
                   Database Resources
@@ -50,7 +64,10 @@ const CreditPage = () => {
                 />
               </div>
             </div>
-            <div className="w-[404px] h-[402px] bg-custom-radial rounded-3xl">
+            <div
+              className="w-[404px] h-[402px] bg-cover bg-center rounded-3xl"
+              style={{ backgroundImage: "url('/radialBg.jpg')" }}
+            >
               <div className="w-[392px] h-[390px] bg-gradient-to-b from-[#1b0e25] via-[#1b0e25] to-[#3a2238] rounded-3xl m-auto mt-[6px]">
                 <p className="text-xl font-sans font-bold text-center pt-8 mb-6">
                   Additional Resources
@@ -82,7 +99,15 @@ const CreditPage = () => {
 
         <div id="team" className="h-screen z-10">
           <div id="title">
-            <h1 className="text-center text-4xl font-sans font-bold mb-7 bg-gradient-to-r from-[#FFC75F] via-[#A8C4FF] to-[#FF6A88] bg-clip-text text-transparent">
+            <h1
+              className="text-center text-4xl font-sans font-bold mb-7 text-transparent bg-clip-text"
+              style={{
+                backgroundImage: "url('/radialBg.jpg')",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                opacity: 0.8,
+              }}
+            >
               Our Team
             </h1>
           </div>
@@ -92,7 +117,11 @@ const CreditPage = () => {
               lot to us~
             </p>
           </div>
-          <div id="member-container" className="h-[609px]  bg-custom-radial">
+          <div
+            id="member-container"
+            className="h-[609px]  bg-cover bg-center mb-[40px]"
+            style={{ backgroundImage: "url('/radialBg.jpg')" }}
+          >
             <div
               id="col1"
               className="flex gap-x-[139px] justify-center mb-7 pt-14"
@@ -182,10 +211,9 @@ const CreditPage = () => {
             </div>
           </div>
         </div>
-        <div id="footer" className="h-[469px] z-10">
-          <p className="text-center text-xl font-sans pt-[94px] mb-[200px]">
-            Thank you to everyone who contributed to making Chat ITP possible!
-          </p>
+
+        {/* <div id="footer" className="h-[469px] z-10">
+          
           <div className="left-0 top-0 flex w-full items-center justify-between">
             <div className="relative m-auto">
               <div className="flex gap-14">
@@ -233,7 +261,7 @@ const CreditPage = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
     </>
   );
