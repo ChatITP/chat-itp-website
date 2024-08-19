c465d44d9903c938ee4593c63ced017fcbf317d6
ziyuan-linn <zl4140@nyu.edu>
2024-08-18 23:01:51 -0400

    multiple blocks

--- components/Workspace/DraggableWrapper.js
+++ components/Workspace/DraggableWrapper.js
@@ -2,13 +2,21 @@ import React from "react";
 import Draggable from "react-draggable";
 import { useRef } from "react";
 
-const DraggableWrapper = ({ children }) => {
+const DraggableWrapper = ({ children, defaultX, defaultY, defaultZ }) => {
   // Reference to the draggable component to supress the findDOMNode warning
   const draggableRef = useRef(null);
-
+  console.log(defaultX, defaultY);
   return (
     <Draggable nodeRef={draggableRef}>
-      <div ref={draggableRef} className="w-fit h-fit">
+      <div
+        ref={draggableRef}
+        className="w-fit h-fit absolute"
+        style={{
+          left: defaultX,
+          top: defaultY,
+          zIndex: defaultZ,
+        }}
+      >
         {children}
       </div>
     </Draggable>
--- components/Workspace/WorkArea.js
+++ components/Workspace/WorkArea.js
@@ -1,5 +1,6 @@
 import DraggableWrapper from "./DraggableWrapper";
 import Block from "./Block";
+import { useState, useRef } from "react";
 
 const testPhrases = [
   "How does",
@@ -11,11 +12,31 @@ const testPhrases = [
 ];
 
 const WorkArea = () => {
+  const [blockList, setBlockList] = useState([]);
+  const zCounterRef = useRef(0);
+
+  const handleDoubleClick = (event) => {
+    setBlockList([
+      ...blockList,
+      {
+        phrases: [],
+        x: event.clientX,
+        y: event.clientY,
+        z: zCounterRef.current++,
+      },
+    ]);
+  };
+
   return (
-    <div className="absolute w-screen h-screen top-0 left-0 overflow-hidden">
-      <DraggableWrapper>
-        <Block initialPromptPhrases={testPhrases} />
-      </DraggableWrapper>
+    <div
+      className="absolute w-screen h-screen top-0 left-0 overflow-hidden"
+      onDoubleClick={handleDoubleClick}
+    >
+      {blockList.map((block, index) => (
+        <DraggableWrapper key={index} defaultX={block.x} defaultY={block.y} defaultZ={block.z}>
+          <Block initialPromptPhrases={block.phrases} />
+        </DraggableWrapper>
+      ))}
     </div>
   );
 };
