38b90c470be7280b9b93083951b7d394387949b7
ziyuan-linn <zl4140@nyu.edu>
2024-08-25 14:38:09 -0400

    change icon

--- components/Workspace/WorkArea.js
+++ components/Workspace/WorkArea.js
@@ -2,24 +2,9 @@ import DraggableWrapper from "./DraggableWrapper";
 import Block from "./Block";
 import { useRecoilState } from "recoil";
 import { blockListState } from "../../contexts/workspace";
-import { useEffect } from "react";
 
 const WorkArea = () => {
   const [blockList, setBlockList] = useRecoilState(blockListState);
-
-  useEffect(() => {
-    const handleKeyDown = (event) => {
-      if (event.key === "Delete") {
-        setBlockList((blockList) => blockList.filter((block) => !block.isSelected));
-      }
-    };
-
-    window.addEventListener("keydown", handleKeyDown);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-    };
-  }, []);
 
   const getHighestZ = () => {
     return blockList.reduce((max, block) => (block.z > max ? block.z : max), 0);
--- components/workspace/Block/Association/Phrase.js
+++ components/workspace/Block/Association/Phrase.js
@@ -1,5 +1,6 @@
-import useClickInOutDetector from "@/hooks/clickInOutDetector";
+import { IoIosAdd } from "react-icons/io";
 import { useRef } from "react";
+import useClickInOutDetector from "@/hooks/clickInOutDetector";
 
 const Phrase = ({
   color,
@@ -46,7 +47,7 @@ const Phrase = ({
           }}
           className="relative font-[600] text-sm text-[#6b6b6b] rounded-md pl-2 break-words cursor-pointer"
         >
-          {isPlaceholder ? "+" : <>{children}</>}
+          {isPlaceholder ? <IoIosAdd className="inline text-lg" /> : <>{children}</>}
         </span>
       );
     }
@@ -60,7 +61,7 @@ const Phrase = ({
         }}
         className="relative font-[600] text-sm text-[#6b6b6b] rounded-md pl-2 pr-6 break-words cursor-pointer"
       >
-        {isPlaceholder ? "+" : <>{children}</>}
+        {isPlaceholder ? <IoIosAdd className="inline text-lg" /> : <>{children}</>}
       </span>
     );
   } else {
@@ -74,7 +75,7 @@ const Phrase = ({
         }}
         className="relative font-[600] text-sm text-black rounded-md pl-2 pr-6 break-words cursor-pointer"
       >
-        {isPlaceholder ? "+" : <>{children}</>}
+        {isPlaceholder ? <IoIosAdd className="inline text-lg" /> : <>{children}</>}
       </span>
     );
   }
