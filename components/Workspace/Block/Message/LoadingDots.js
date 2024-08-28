f08acfd5f2b97474802ad10a822a6f9ea9837721
Ziyuan Lin <zl4140@nyu.edu>
2024-08-27 21:01:46 -0400

    Delete components/Workspace directory

--- components/Workspace/Block/Message/LoadingDots.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { BeatLoader } from "react-spinners";
-
-const LoadingDots = ({ color = "#aaaaaa" }) => {
-  return <BeatLoader color={color} />;
-};
-
-export default LoadingDots;
