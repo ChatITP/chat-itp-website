caed0be3d846b0b7178f2feb4cb32f73bcabb414
Ziyuan Lin <zl4140@nyu.edu>
2024-09-23 00:25:12 -0400

    update block creation and ordering

--- contexts/workspace.js
+++ contexts/workspace.js
@@ -1,8 +1,25 @@
 import { atom } from "recoil";
+import uniqid from "uniqid";
 
 const blockListState = atom({
   key: "blockList",
   default: [],
 });
 
-export { blockListState };
+function createBlock(type = "block", x = 0, y = 0, z = 0, phrases = [], dragFlag = false) {
+  return {
+    x,
+    y,
+    z,
+    id: uniqid(),
+    type,
+    phrases,
+    dragFlag,
+  };
+}
+
+function getHighestZ(blockList) {
+  return blockList.reduce((max, block) => (block.z > max ? block.z : max), 0);
+}
+
+export { createBlock, getHighestZ, blockListState };
