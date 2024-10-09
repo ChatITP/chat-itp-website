3b748169341099182ed171ca4ab020cae84f69a0
nina-jsl <145054713+nina-jsl@users.noreply.github.com>
2024-10-09 17:28:46 -0400

    edited handlerefreshexample function

--- components/Workspace/Examples/ExampleCardsController.js
+++ components/Workspace/Examples/ExampleCardsController.js
@@ -5,7 +5,12 @@ import shuffle from "@/utils/shuffle";
 import { FaAngleDoubleUp } from "react-icons/fa";
 import { FaAngleDoubleDown } from "react-icons/fa";
 import { FaArrowsRotate } from "react-icons/fa6";
+import promptData from "public/prompt_data.json";
 
+//function to pick 10 random items from json file
+const pickRandomPrompts = (prompts, count) => {
+  return [...prompts].sort(() => 0.5 - Math.random()).slice(0, count);
+};
 const ExampleCardsController = () => {
   const [showExamples, setShowExamples] = useRecoilState(showExamplesState);
   const setExamples = useSetRecoilState(exampleState);
@@ -15,11 +20,7 @@ const ExampleCardsController = () => {
   };
 
   const handleRefreshExamples = () => {
-    setExamples((prev) => {
-      const newExamples = [...prev];
-      shuffle(newExamples);
-      return newExamples;
-    });
+    setExamples(() => pickRandomPrompts(promptData, 10));
   };
 
   return (
