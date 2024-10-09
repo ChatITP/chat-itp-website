50504a2fc3421ad72d9d0abada632dd56678cc74
nina-jsl <145054713+nina-jsl@users.noreply.github.com>
2024-10-09 17:23:47 -0400

    included json file

--- contexts/examples.js
+++ contexts/examples.js
@@ -10,71 +10,20 @@ const showExamplesState = atom({
   default: true,
 });
 
+//import json file
+import promptData from "public/prompt_data.json";
+
+// function to shuffle and pick 10 random prompts
+const pickRandomPrompts = (prompts, count) => {
+  return [...prompts].sort(() => 0.5 - Math.random()).slice(0, count);
+};
+
 const exampleState = atom({
   key: "examples",
-  default: [
-    {
-      text: "What's the most innovative theme of ITP Projects?",
-      phrases: ["What's", "the most innovative", "theme", "of ITP Projects?"],
-    },
-    {
-      text: "Describe the most innovative 3d game modeling projects from the 1980s that utilized early computer graphics.",
-      phrases: [
-        "Describe",
-        "the most innovative",
-        "3d game modeling projects",
-        "from the 1980s",
-        "that utilized early computer graphics.",
-      ],
-    },
-    {
-      text: "In 2075, ITP students will use AR to redefine social interactions. Describe a project and its implications on privacy and connections.",
-      phrases: [
-        "In 2075,",
-        "ITP students",
-        "will use AR",
-        "to redefine social interactions.",
-        "Describe",
-        "a project",
-        "and its implications on privacy and connections.",
-      ],
-    },
-    {
-      text: "Imagine that all the ITP student work represents the DNA of a university program. Describe a university program based on this DNA.",
-      phrases: [
-        "Imagine",
-        "that all the ITP student work",
-        "represents the DNA",
-        "of a university program.",
-        "Describe",
-        "a university program",
-        "based on this DNA.",
-      ],
-    },
-    {
-      text: "Write an advertisement poem about ITP Spring Show.",
-      phrases: ["Write", "an advertisement poem", "about ITP Spring Show."],
-    },
-
-    {
-      text: "How did ITP projects evolve in terms of multimedia storytelling from the 2000s to the 2010s?",
-      phrases: [
-        "How did ITP projects evolve",
-        "in terms of multimedia storytelling",
-        "from the 2000s",
-        "to the 2010s?",
-      ],
-    },
-    {
-      text: "Design the “ITP version” of these common objects: Toaster, bubble tea, supercomputer, paper clip, pet chihuahua, underpants.",
-      phrases: [
-        "Design",
-        "the “ITP version”",
-        "of these common objects:",
-        "Toaster, bubble tea, supercomputer, paper clip, pet chihuahua, underpants.",
-      ],
-    },
-  ],
+  default: pickRandomPrompts(promptData, 10).map(prompt => ({
+    text: prompt.text,
+    phrases: prompt.text.split(/[\s,]+/).map(word => word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,""))
+  })),
 });
 
 export { tagState, showExamplesState, exampleState };
