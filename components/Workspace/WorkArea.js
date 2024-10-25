6dc8f9c20676164cca5572cf1739e4375cdc2f95
Ziyuan Lin <zl4140@nyu.edu>
2024-10-25 18:09:51 -0400

    mouse support

--- components/Workspace/WorkArea.js
+++ components/Workspace/WorkArea.js
@@ -8,23 +8,64 @@ const WorkArea = () => {
   const [blockList, setBlockList] = useRecoilState(blockListState);
   const [viewportPosition, setViewportPosition] = useRecoilState(viewportPositionState);
   const prevPos = useRef({ x: 0, y: 0 });
+  const mousePosRef = useRef({ x: 0, y: 0 });
 
   // Disables default pinch zooming on track pads
+  // Enables zooming in and out with ctrl + plus or minus
   useEffect(() => {
     const noPinchZoom = (event) => {
       event.preventDefault();
     };
 
+    const controlZoom = (event) => {
+      if (event.ctrlKey) {
+        event.preventDefault();
+        if (event.key === "=") {
+          const newScale = Math.min(2, viewportPosition.scale + 0.4);
+          let canvasMouseX = mousePosRef.current.x / viewportPosition.scale + viewportPosition.x;
+          let canvasMouseY = mousePosRef.current.y / viewportPosition.scale + viewportPosition.y;
+
+          let newVX = canvasMouseX - mousePosRef.current.x / newScale;
+          let newVY = canvasMouseY - mousePosRef.current.y / newScale;
+
+          setViewportPosition(() => {
+            return {
+              x: newVX,
+              y: newVY,
+              scale: newScale,
+            };
+          });
+        } else if (event.key === "-") {
+          const newScale = Math.max(0.1, viewportPosition.scale - 0.4);
+          let canvasMouseX = mousePosRef.current.x / viewportPosition.scale + viewportPosition.x;
+          let canvasMouseY = mousePosRef.current.y / viewportPosition.scale + viewportPosition.y;
+
+          let newVX = canvasMouseX - mousePosRef.current.x / newScale;
+          let newVY = canvasMouseY - mousePosRef.current.y / newScale;
+
+          setViewportPosition(() => {
+            return {
+              x: newVX,
+              y: newVY,
+              scale: newScale,
+            };
+          });
+        }
+      }
+    };
+
     window.addEventListener("wheel", noPinchZoom, { passive: false });
+    window.addEventListener("keydown", controlZoom, { passive: false });
     return () => {
       window.removeEventListener("wheel", noPinchZoom);
+      window.removeEventListener("keydown", controlZoom);
     };
   });
 
   const onWheel = (event) => {
-    event.preventDefault();
     if (event.ctrlKey) {
-      const dScale = event.deltaY * -0.01;
+      const direction = event.deltaY > 0 ? -1 : 1;
+      const dScale = Math.min(10, Math.abs(event.deltaY)) * -0.01 * direction;
       const newScale = Math.max(0.1, Math.min(2, viewportPosition.scale + dScale));
 
       let canvasMouseX = event.clientX / viewportPosition.scale + viewportPosition.x;
@@ -81,11 +122,16 @@ const WorkArea = () => {
     window.removeEventListener("mouseup", onMouseUp);
   };
 
+  const onMouseMove = (event) => {
+    mousePosRef.current = { x: event.clientX, y: event.clientY };
+  };
+
   return (
     <div
       className="absolute w-screen h-screen top-0 left-0 overflow-hidden -z-10"
       onMouseDown={onMouseDown}
       onWheel={onWheel}
+      onMouseMove={onMouseMove}
     >
       {blockList.map((block) => (
         <DraggableWrapper block={block} viewport={viewportPosition}>
