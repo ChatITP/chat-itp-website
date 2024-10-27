a93096ed95b3e578609a47b456853a503a515b66
nina-jsl <145054713+nina-jsl@users.noreply.github.com>
2024-10-27 18:05:45 -0400

    delete with cross

--- components/Workspace/Block/index.js
+++ components/Workspace/Block/index.js
@@ -3,25 +3,42 @@ import Association from "./Association";
 import Message from "./Message";
 import { blockListState } from "@/contexts/workspace";
 import { useRecoilState } from "recoil";
+import { IoClose } from "react-icons/io5";
 
 const Block = ({ initialPromptPhrases, id }) => {
   const [blockList, setBlockList] = useRecoilState(blockListState);
   const promptRef = useRef("");
 
-  const handleKeyDown = (event) => {
-    if (event.key === "Backspace" || event.key === "Delete") {
-      event.preventDefault();
-      event.stopPropagation();
-      setBlockList((prevList) => {
-        return prevList.filter((block) => block.id !== id);
-      });
-    }
+  const handleDelete = () => {
+    setBlockList((prevList) => {
+      return prevList.filter((block) => block.id !== id);
+    });
   };
+
+  // const handleKeyDown = (event) => {
+  //   if (event.key === "Backspace" || event.key === "Delete") {
+  //     event.preventDefault();
+  //     event.stopPropagation();
+  //     setBlockList((prevList) => {
+  //       return prevList.filter((block) => block.id !== id);
+  //     });
+  //   }
+  // };
   return (
     <div
-      className="bg-gray w-[550px] rounded-xl drop-shadow-lg flex flex-col"
-      onKeyDown={handleKeyDown}
+      className="relative bg-gray w-[550px] rounded-xl drop-shadow-lg flex flex-col"
+      // onKeyDown={handleKeyDown}
     >
+      <button
+        onClick={(e) => {
+          e.stopPropagation();
+          console.log("Delete button clicked");
+          handleDelete();
+        }}
+        className="absolute z-20 top-2 right-2 text-white hover:text-red-500"
+      >
+        <IoClose size={20} />
+      </button>
       <Association promptRef={promptRef} initialPromptPhrases={initialPromptPhrases} />
       <Message promptRef={promptRef} />
     </div>
